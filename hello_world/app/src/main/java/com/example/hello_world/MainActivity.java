4e588875af285e3b9fb1320cab4e2e57f3a1f399
Pratik Adarsh <pratikadarsh24@gmail.com>
2019-10-11 07:15:37 +0530

    first commit

---
+++ hello_world/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Main Activity", "Hello World");
+    }
+}
